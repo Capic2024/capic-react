fb678d2a73e3d9e19df41205a92b7752f6f69dd5
sengooooo <lesley023@naver.com>
2024-04-16 19:16:30 +0900

    test : s3 bucket delete test

--- capic/src/result/result.jsx
+++ capic/src/result/result.jsx
@@ -5,7 +5,7 @@ import Mosic from '../result/mosic';
 import Timeline from '../result/timeline';
 import filedown from '../image/FilePlus.svg';
 import down from '../image/Download.svg';
-
+import AWS from 'aws-sdk';
 
 
 const Wrapper=styled.div`
@@ -39,28 +39,30 @@ const ButtonContainer = styled.div`
     margin-top : 3.75rem;
 `;
 
-const FilePlus = styled.div`
-border-radius: 0.5rem;
+const FilePlus = styled.button`
+    border-radius: 0.5rem;
     background: var(--Primary-color, #BF92FB);
     display: flex;
     width: 12.5rem;
     padding: 0.875rem 1.625rem;
     align-items: center;
     gap: 0.25rem;
-
+    FilePlus:hover, FilePlus:hover > * {
+        cursor: pointer;
+    }
 `;
 
-const Download = styled.div`
-display: flex;
-width: 12.5rem;
-height: 3.75rem;
-padding: 0rem 3.125rem;
-align-items: center;
-gap: 0.25rem;
-flex-shrink: 0;
-border-radius: 0.5rem;
-background: var(--Gray8, #191B24);
-margin-left : 1.5rem;
+const Download = styled.button`
+    display: flex;
+    width: 12.5rem;
+    height: 3.75rem;
+    padding: 0rem 3.125rem;
+    align-items: center;
+    gap: 0.25rem;
+    flex-shrink: 0;
+    border-radius: 0.5rem;
+    background: var(--Gray8, #191B24);
+    margin-left : 1.5rem;
 `;
 
 const Font = styled.div`
@@ -74,30 +76,79 @@ const Font = styled.div`
     line-height: normal;
 `
 
-function Result(){
-    return(
-        <>
-        <Wrapper>
-            <HeaderComponent>
-                <Logo src={logo}></Logo>
-            </HeaderComponent>
-            <Mosic/>
-            <Timeline/>
-            <ButtonContainer>
-                <FilePlus>
-                    <img src={filedown}/>
-                    <Font>다른 영상 만들기</Font>
-                </FilePlus>
-                    
-                <Download>
-                    <img src={down}/>
-                    <Font>다운로드</Font>
-                </Download>
-            </ButtonContainer>
-        </Wrapper>
-    </>
-    );
+function Result() {
+    AWS.config.update({
+        region: process.env.REACT_APP_region,
+        credentials: new AWS.Credentials(process.env.REACT_APP_accessKeyId, process.env.REACT_APP_secretAccessKey)
+    });
 
+    const s3 = new AWS.S3();
+
+    async function deleteFolderContents(bucketName, folderPrefix) {
+        try {
+            // 폴더 내의 객체 목록 조회
+            const listParams = {
+                Bucket: bucketName,
+                Prefix: folderPrefix
+            };
+
+            
+            const listedObjects = await s3.listObjectsV2(listParams).promise();
+
+            if (listedObjects.Contents.length > 0) {
+                const deleteParams = {
+                    Bucket: bucketName,
+                    Delete: {
+                        Objects: listedObjects.Contents.map(({ Key }) => ({ Key })),
+                    }
+                };
+                console.log("Deleting the following objects:", deleteParams.Delete.Objects);
+                const response = await s3.deleteObjects(deleteParams).promise();
+                console.log("Delete response:", response);
+                if (listedObjects.IsTruncated) {
+                    await deleteFolderContents(bucketName, folderPrefix);
+                }
+            } else {
+                console.log("No objects to delete in the specified folder.");
+            }
+
+        } catch (err) {
+            console.error("Error in deleting folder contents:", err);
+        }
+    }
+
+    const handleDelete = async () => {
+        const bucketName = process.env.REACT_APP_bucket;
+        const folderPrefix = sessionStorage.getItem("uuid") + "/";
+        console.log("버킷명 : "+bucketName);
+        console.log("폴더명 : "+folderPrefix);
+        await deleteFolderContents(bucketName, folderPrefix);
+        console.log('Folder contents deleted successfully');
+    };
+
+    
+    return (
+        <>
+            <Wrapper>
+                <HeaderComponent>
+                    <Logo src={logo}></Logo>
+                </HeaderComponent>
+                <Mosic/>
+                <Timeline/>
+                <ButtonContainer>
+                    <FilePlus onClick={handleDelete}>
+                        <img src={filedown}/>
+                        <Font>다른 영상 만들기</Font>
+                    </FilePlus>
+                    
+                    <Download onClick={handleDelete}>
+                        <img src={down}/>
+                        <Font>다운로드</Font>
+                    </Download>
+                </ButtonContainer>
+            </Wrapper>
+        </>
+    );
 }
 
 export default Result;
